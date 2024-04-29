3ddfc01bce08ceac368833ffa94f02a452765b9d
HexedHero <6012891+HexedHero@users.noreply.github.com>
2024-04-29 18:52:36 +0100

    1.20.5 -> 1.20.6

--- NMSInstaller.bat
+++ NMSInstaller.bat
@@ -4,7 +4,7 @@
 set java8=1.8 1.8.3 1.8.8 1.9.2 1.9.4 1.10.2 1.11.2 1.12.2 1.13 1.13.2 1.14.4 1.15.2 1.16.1 1.16.3 1.16.5
 set java16=1.17.1
 set java17=1.18.1 1.18.2 1.19.2 1.19.3 1.19.4 1.20.1 1.20.2 1.20.4
-set java21=1.20.5
+set java21=1.20.6
 
 :: Set JDK versions (https://adoptium.net/temurin/releases/)
 set java8version=jdk8u402-b06
